774838eb306bd438691303338dbd65a7667a2502
nshaw973 <nshaw973@gmail.com>
2023-03-31 18:34:12 -0700

    got the seeded blog post to populate into the main page, next is to create a login

--- seeds/blogData.js
+++ seeds/blogData.js
@@ -5,7 +5,7 @@ const blogData = [
     {
         name: 'Nathan',
         title: 'Welcome!',
-        post: `Welcome to the Blog! Make sure to create an account and after that, you'll be able to creat youur own blogs!`,
+        post: `Welcome to the Blog! Make sure to create an account and after that, you'll be able to creat your own blogs!`,
         user_id: 1
     }
 ]
