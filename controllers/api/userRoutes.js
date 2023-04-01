3e6fa9ee51e4a35be0531d09bd370f1a3c746abe
nshaw973 <nshaw973@gmail.com>
2023-03-31 20:01:37 -0700

    added login functionality.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,41 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
-
+    try {
+        const userData = await User.create(req.body);
+    
+        req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+    
+          res.status(200).json(userData);
+        });
+      } catch (err) {
+        res.status(400).json(err);
+      };
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+        if (!userData) {
+            window.prompt('Error with login, check email or password and try again')
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            window.prompt('Error with login, check email or password and try again')
+            return;
+        }
+        req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+          
+          res.redirect('/');
+        });
+      } catch (err) {
+        res.status(400).json(err);
+      }
+})
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,20 +1,29 @@
 const router = require('express').Router();
-const { Blog, User } = require('../models')
+const { Blog, User } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
         const blogData = await Blog.findAll({
             include: [{ model: User, attributes: ['name'] }]
-        })
+        });
         const blogPosts = blogData.map((posts) => posts.get({ plain: true }));
 
         res.render('homepage', {
-            blogPosts
-        })
+            blogPosts,
+            logged_in: req.session.logged_in
+        });
     } catch (err) {
         res.status(500)
         console.log(err)
-    }
-})
+    };
+});
+
+router.get('/login', (req, res) => {
+    try {
+        res.render('login')
+    } catch(err) {
+        res.redirect('/');
+    };
+});
 
 module.exports = router;
